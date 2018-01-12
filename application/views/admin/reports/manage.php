5fd356fef9427e5f0087e3131131b4f754e47100
nicksweb <nosullivan92@gmail.com>
2018-01-13 08:39:02 +1000

    Chosen JS - Moved to Template.php (Admin)
    Removed chosen select Javascript coodes from views in admin and
    integrated into template.php instead.

--- application/views/admin/reports/manage.php
+++ application/views/admin/reports/manage.php
@@ -62,21 +62,6 @@
                         </div>
                     </th>
 					
-					 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js" type="text/javascript"></script>
-  <script src="<?php echo base_url(); ?>chosen.jquery.js" type="text/javascript"></script>
-  <script src="<?php echo base_url(); ?>docsupport/prism.js" type="text/javascript" charset="utf-8"></script>
-  <script type="text/javascript">
-    var config = {
-      '.chosen-select'           : {},
-      '.chosen-select-deselect'  : {allow_single_deselect:true},
-      '.chosen-select-no-single' : {disable_search_threshold:10},
-      '.chosen-select-no-results': {no_results_text:'Oops, nothing found!'},
-      '.chosen-select-width'     : {width:"95%"}
-    }
-    for (var selector in config) {
-      $(selector).chosen(config[selector]);
-    }
-  </script>	
 					
                 <?php echo form_close(); ?>
             </tr>
